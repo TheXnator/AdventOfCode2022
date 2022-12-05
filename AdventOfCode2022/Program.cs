dd4cd4b7ff7501f5642bf44a879d21ccccedba41
TheXnator <thexnator@thexnator.dev>
2022-12-05 19:36:57 +0000

    day 2 part 1

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,6 +1,8 @@
 ﻿static void Run() {
     Console.WriteLine(String.Format("Day 1 part 1: {0}", GetMostCalories()));
     Console.WriteLine(String.Format("Day 1 part 2: {0}", GetMostCalories(true)));
+
+    Console.WriteLine(String.Format("Day 2 part 1: {0}", GetRockPaperScissors()));
 }
 
 static int GetMostCalories(bool top3=false)
@@ -24,6 +26,40 @@ static int GetMostCalories(bool top3=false)
     allvals = allvals.OrderByDescending(c => c).ToList();
 
     return top3 ? (allvals[0] + allvals[1] + allvals[2]) : allvals[0];
+}
+
+static int GetRockPaperScissors() {
+    string filename = "day2inputs.txt";
+    int score = 0;
+
+    Dictionary<char, char> wins = new Dictionary<char, char>();
+    wins.Add('X', 'C');
+    wins.Add('Y', 'A');
+    wins.Add('Z', 'B');
+
+    Dictionary<char, int> choiceScores = new Dictionary<char, int>();
+    choiceScores.Add('A', 1);
+    choiceScores.Add('B', 2);
+    choiceScores.Add('C', 3);
+    choiceScores.Add('X', 1);
+    choiceScores.Add('Y', 2);
+    choiceScores.Add('Z', 3);
+
+    string[] contents = File.ReadAllLines(filename);
+    foreach (string line in contents)
+    {
+        char opponentchoice = line[0];
+        char ownchoice = line[2];
+
+        // Apply win-loss score
+        int winloss = (choiceScores[opponentchoice] == choiceScores[ownchoice] ? 3 : (wins[ownchoice] == opponentchoice ? 6 : 0));
+        score = score + winloss;
+        // Apply choice score
+        int choiceScore = choiceScores[ownchoice];
+        score = score + choiceScore;
+    }
+
+    return score;
 }
 
 Run();
