384ce627704936aeda07a17eade2a6164cb4111e
TheXnator <thexnator@thexnator.dev>
2022-12-05 19:08:39 +0000

    day 1 part 2

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,12 +1,13 @@
 ﻿static void Run() {
     Console.WriteLine(String.Format("Day 1 part 1: {0}", GetMostCalories()));
+    Console.WriteLine(String.Format("Day 1 part 2: {0}", GetMostCalories(true)));
 }
 
-static int GetMostCalories()
+static int GetMostCalories(bool top3=false)
 {
     string filename = "day1inputs.txt";
-    int maxval = 0;
     int curval = 0;
+    List<int> allvals = new List<int>();
 
     string[] contents = File.ReadAllLines(filename);
     foreach (string line in contents)
@@ -15,15 +16,14 @@ static int GetMostCalories()
             int lineval = Convert.ToInt32(line);
             curval = curval + lineval;
         } catch (Exception e) {
-            if (curval > maxval) {
-                maxval = curval;
-            }
-
+            allvals.Add(curval);
             curval = 0;
         }
     }
 
-    return maxval;
+    allvals = allvals.OrderByDescending(c => c).ToList();
+
+    return top3 ? (allvals[0] + allvals[1] + allvals[2]) : allvals[0];
 }
 
 Run();
