bd66892aac1feb3ca60b7c79002cc462277f29f9
TheXnator <thexnator@thexnator.dev>
2022-12-07 18:52:36 +0000

    day 2 part 2

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -3,6 +3,7 @@
     Console.WriteLine(String.Format("Day 1 part 2: {0}", GetMostCalories(true)));
 
     Console.WriteLine(String.Format("Day 2 part 1: {0}", GetRockPaperScissors()));
+    Console.WriteLine(String.Format("Day 2 part 2: {0}", GetRockPaperScissorsLosses()));
 }
 
 static int GetMostCalories(bool top3=false)
@@ -53,6 +54,47 @@ static int GetRockPaperScissors() {
 
         // Apply win-loss score
         int winloss = (choiceScores[opponentchoice] == choiceScores[ownchoice] ? 3 : (wins[ownchoice] == opponentchoice ? 6 : 0));
+        score = score + winloss;
+        // Apply choice score
+        int choiceScore = choiceScores[ownchoice];
+        score = score + choiceScore;
+    }
+
+    return score;
+}
+
+static int GetRockPaperScissorsLosses()
+{
+    string filename = "day2inputs.txt";
+    int score = 0;
+
+    Dictionary<char, char> wins = new Dictionary<char, char>();
+    wins.Add('A', 'B');
+    wins.Add('B', 'C');
+    wins.Add('C', 'A');
+
+    Dictionary<char, char> losses = new Dictionary<char, char>();
+    losses.Add('A', 'C');
+    losses.Add('B', 'A');
+    losses.Add('C', 'B');
+
+    Dictionary<char, int> choiceScores = new Dictionary<char, int>();
+    choiceScores.Add('A', 1);
+    choiceScores.Add('B', 2);
+    choiceScores.Add('C', 3);
+
+    string[] contents = File.ReadAllLines(filename);
+    foreach (string line in contents)
+    {
+        char opponentchoice = line[0];
+        char desiredresult = line[2];
+
+        char winchoice = wins[opponentchoice];
+        char losschoice = losses[opponentchoice];
+        char ownchoice = desiredresult == 'X' ? losschoice : (desiredresult == 'Y' ? opponentchoice : winchoice);
+
+        // Apply win-loss score
+        int winloss = (desiredresult == 'Y' ? 3 : (desiredresult  == 'Z' ? 6 : 0));
         score = score + winloss;
         // Apply choice score
         int choiceScore = choiceScores[ownchoice];
